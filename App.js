a635f35d96295d3b1952e8988b3501081e247dd7
son nguyen <son.nguyen@nfq.asia>
2021-01-14 18:51:52 +0700

    wip: side slider

--- App.js
+++ App.js
@@ -6,24 +6,29 @@
  * @flow strict-local
  */
 
-import Slider from '@react-native-community/slider';
 import React, {useRef, useState} from 'react';
 import {
   Animated,
   Dimensions,
+  Pressable,
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import VerticalSlider from 'rn-vertical-slider';
+
 const WIDTH = Dimensions.get('screen').width;
 
 const SQUARE_SIZE = 50;
 const MAX_DURATION = 2000; // 2s
-const STEPS = 5;
+const STEPS = MAX_DURATION / 4;
+const SLIDER_WIDTH = WIDTH * 0.8;
+const SLIDER_HEIGHT = 70;
+const SETTING_BUTTON_SIZE = 40;
+const SPEED_INDICATOR_WIDTH = 90;
 
 const SPEEDS = {
   0: 'Fastest',
@@ -36,7 +41,11 @@ const SPEEDS = {
 const App: () => React$Node = () => {
   const coord = useRef(new Animated.ValueXY({x: 200, y: 200})).current;
   const rotate = useRef(new Animated.Value(0)).current;
+  const sliderAnimatedValue = useRef(new Animated.Value(0)).current; // slider hiding
+
   const [speed, setSpeed] = useState(MAX_DURATION / 2);
+  const speedAnimatedValue = useRef(new Animated.Value(speed)).current;
+  const [isOpenSlider, setIsOpenSlider] = useState(false);
 
   const handleTouch = (event) => {
     const {locationX: x, locationY: y} = event.nativeEvent;
@@ -57,7 +66,17 @@ const App: () => React$Node = () => {
   };
 
   const handleSliderValueChanged = (value) => {
-    setSpeed(value || 80); // fast enough to be snappy, but no so fast as instantly (0)
+    setSpeed(value);
+    speedAnimatedValue.setValue(value);
+  };
+
+  const handleToggleSpeedSlider = () => {
+    Animated.timing(sliderAnimatedValue, {
+      toValue: isOpenSlider ? 0 : 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+    setIsOpenSlider(!isOpenSlider);
   };
 
   return (
@@ -65,12 +84,11 @@ const App: () => React$Node = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{flex: 1}}>
         <View style={styles.container}>
+          {/* TOUCH ZONE */}
           <TouchableOpacity
             activeOpacity={1}
             onPress={handleTouch}
             style={styles.touchZone}>
-            <Text style={{textAlign: 'center', margin: 10}}>Touch Zone</Text>
-
             <Animated.View
               style={{
                 ...styles.square,
@@ -91,27 +109,65 @@ const App: () => React$Node = () => {
             />
           </TouchableOpacity>
 
-          <View style={styles.sliderContainer}>
-            <View style={styles.stepHintContainer}>
-              {Array.from(Array(STEPS), (e, index) => (
-                <View key={index} style={styles.dot}>
-                  <Text style={styles.stepIndicator}>{SPEEDS[index]}</Text>
-                </View>
-              ))}
-            </View>
-            <Slider
-              style={styles.slider}
-              minimumValue={0}
-              maximumValue={MAX_DURATION}
-              minimumTrackTintColor="#bf0000"
-              maximumTrackTintColor="#000000"
-              thumbTintColor="#7b7cac"
-              step={MAX_DURATION / (STEPS - 1)}
-              tapToSeek={true}
+          {/* SPEED SETTING SLIDER FROM EDGE */}
+          <Animated.View
+            style={{
+              ...styles.sliderSettingContainer,
+              ...styles.shadow,
+              transform: [
+                {
+                  translateX: sliderAnimatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-SLIDER_HEIGHT, 0],
+                  }),
+                },
+              ],
+            }}>
+            {/* SLIDER */}
+            <VerticalSlider
               value={speed}
-              onValueChange={handleSliderValueChanged}
+              min={0}
+              max={MAX_DURATION}
+              onChange={handleSliderValueChanged}
+              width={SLIDER_HEIGHT}
+              height={SLIDER_WIDTH}
+              step={STEPS}
+              minimumTrackTintColor={'gray'}
+              maximumTrackTintColor={'tomato'}
+              showBallIndicator={false}
+              ballIndicatorPosition={0}
+              borderRadius={0}
             />
-          </View>
+
+            {/* <View
+              style={{
+                width: SPEED_INDICATOR_WIDTH,
+                overflow: 'hidden',
+                marginLeft: 8,
+              }}>
+              <Animated.View
+                style={{
+                  flexDirection: 'row',
+                  transform: [
+                    {
+                      translateX: (speed / STEPS) * -SPEED_INDICATOR_WIDTH,
+                    },
+                  ],
+                }}>
+                {Object.values(SPEEDS).map((e, i) => (
+                  <Text key={i} style={styles.stepIndicator}>
+                    {e}
+                  </Text>
+                ))}
+              </Animated.View>
+            </View> */}
+            {/* SETTING BUTTON */}
+            <Pressable
+              style={styles.settingBtn}
+              onPress={handleToggleSpeedSlider}>
+              <Icon name="settings" size={24} />
+            </Pressable>
+          </Animated.View>
         </View>
       </SafeAreaView>
     </>
@@ -136,26 +192,59 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   slider: {
-    width: WIDTH * 0.8,
-    height: 40,
-  },
-  sliderContainer: {
-    marginTop: 20,
-  },
-  stepHintContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    ...StyleSheet.absoluteFill,
-    bottom: 60,
-  },
-  dot: {
-    height: 40,
-    width: (WIDTH * 0.8) / 6,
+    width: SLIDER_WIDTH,
+    height: SLIDER_HEIGHT,
+    backgroundColor: '#fff',
+    transform: [
+      {rotate: '-90deg'},
+      {
+        translateY: -(
+          SLIDER_WIDTH / 2 +
+          SLIDER_HEIGHT / 2 +
+          SETTING_BUTTON_SIZE
+        ),
+      },
+      {
+        translateX: -(SLIDER_WIDTH / 2 - SLIDER_HEIGHT / 2),
+      },
+    ],
   },
   stepIndicator: {
-    color: '#000',
+    letterSpacing: 1.2,
+    height: 20,
+    fontWeight: 'bold',
+    width: SPEED_INDICATOR_WIDTH,
     textAlign: 'center',
+    backgroundColor: 'red',
+  },
+  settingBtn: {
+    backgroundColor: '#fff',
+    width: SETTING_BUTTON_SIZE,
+    height: SETTING_BUTTON_SIZE,
+
+    borderTopEndRadius: 20,
+    borderBottomEndRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sliderSettingContainer: {
+    position: 'absolute',
+    top: 100,
+    left: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
   },
 });
 
